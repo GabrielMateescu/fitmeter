6482aa74c8d04c05ee2fe53eb802dbaabbe886a3
Gabriel Mateescu <gabriel-cosmin.mateescu@haufe-lexware.com>
2017-12-13 11:22:52 +0200

    First Commit

---
+++ src/main/java/com/fitmeter/fitmeter/FitmeterApplication.java
@@ -0,0 +1,12 @@
+package com.fitmeter.fitmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmeterApplication.class, args);
+	}
+}
